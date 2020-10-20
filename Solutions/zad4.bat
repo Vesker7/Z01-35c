ac0a074e7f71232297366b529b020af1da75a71e
Vesker7 <61987610+Vesker7@users.noreply.github.com>
2020-10-20 21:54:57 +0200

    Add zad4

---
+++ Solutions/zad4.bat
@@ -0,0 +1,18 @@
+@echo off
+if %1 LSS 1 (exit /b)
+
+setlocal enabledelayedexpansion
+
+set /a prev = 1
+set /a curr = 1
+echo %prev%
+
+
+for /l %%i in (1,1,%1) do (
+	set /a next = !prev! + !curr!
+	set /a prev = !curr!
+	echo !curr!
+	set /a curr = !next!
+)
+
+endlocal
